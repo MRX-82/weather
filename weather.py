f2b3b229d35353476e5f5a1b2615b564b5dad83f
Luky1987 <82019.is@gmail.com>
2024-01-31 17:37:03 +0300

    Прогноз погоды для телеграмм бота. В зависимости от команды опрелеляет идентификатор искомого города и выдает прогноз погоды текущий или на 5 дней.

--- weather.py
+++ weather.py
@@ -6,17 +6,60 @@ s_city = lg.s_city
 city_id = lg.city_id
 appid = lg.appid
 
-try:
-    res = requests.get("http://api.openweathermap.org/data/2.5/find",
-                       params = {'q': s_city, 'type': 'like', 'units': 'metric',
-                                 'APPID': appid})
-    data = res.json()
-    cityes = ['{} ({})'.format(d['name'], d['sys']['country'])
-              for d in data['list']]
-    print('city:', cityes)
-    city_id = data['list'][0]['id']
-    print('city_id:', city_id)
 
-except Exception as e:
-    print('Exception (find):', e)
-    pass
+def wea_serch():
+    """
+    Weather on serch city
+    """
+
+    try:
+        res = requests.get("http://api.openweathermap.org/data/2.5/find",
+                           params = {'q': s_city, 'type': 'like', 'units': 'metric',
+                                     'APPID': appid})
+        data = res.json()
+        cityes = ['{} ({})'.format(d['name'], d['sys']['country'])
+                  for d in data['list']]
+        print('city:', cityes)
+        city_id = data['list'][0]['id']
+        print('city_id:', city_id)
+
+    except Exception as e:
+        print('Exception (find):', e)
+        pass
+
+
+def wea_five_day():
+    """
+    Weather on 5 day
+    """
+    
+    try:
+        res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                       params={'id': city_id, 'units': 'metric', 'lang': 'ru',
+                               'APPID': appid})
+        data=res.json()
+        for i in data['list']:
+            print(i['dt_txt'], '{0:+3.0f}'.format(i['main']['temp']), i['weather'][0]['description'])
+
+    except Exception as e:
+        print("Exception (forecast)", e)
+        pass
+
+
+def wea_day():
+    """
+    Weather today
+    """
+    try:
+        res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                     params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+        data = res.json()
+        print("conditions:", data['weather'][0]['description'])
+        print("temp:", data['main']['temp'])
+        print("temp_min:", data['main']['temp_min'])
+        print("temp_max:", data['main']['temp_max'])
+    except Exception as e:
+        print("Exception (weather):", e)
+        pass
+
+weather = wea_five_day()
